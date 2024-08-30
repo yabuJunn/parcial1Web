c0f970eef7073e85845d2049b881e74260967384
yabuJunn <juancho04reyes@gmail.com>
2024-08-29 19:31:52 -0500

    Total

--- src/components/PurchaseSummary/PurchaseSummary.jsx
+++ src/components/PurchaseSummary/PurchaseSummary.jsx
@@ -3,21 +3,19 @@ import './PurchaseSummary.css'
 export const PurchaseSummary = ({categories}) => {
     console.log(categories)
 
-    console.log(categories[0])
-
     const value1 = categories[0]
     
     const value2 = categories[1]
 
     const value3 = categories[2]
 
-    const total = value1[1] + value2[2] + value3[3] 
+    const total = categories.category1[1] + categories.category2[1] + categories.category3[1]
 
     console.log(total)
 
     return <>
         <div id="purchaseSummary">
-            <p></p>
+            <p>Total es: {total}</p>
         </div>
     </>
 }
